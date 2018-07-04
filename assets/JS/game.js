cac99288ac44f98a803daa6467231db0907daaf4
Acher249 <jchernick2010@gmail.com>
2018-07-03 21:29:33 -0400

    Added Project

---
+++ assets/JS/game.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+
+$(".number").on("click", function() {
+
+  //do somthing
+  
+});
+
+$("#result").text(result);
+
+
+// swal({
+//   title: "YOU WIN!",
+//   icon: "success",
+//   text: "This is subtext!",
+//   button: "Good Work", 
+// });
