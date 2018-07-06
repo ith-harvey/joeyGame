29f445ae0d0e24fbfb5be1c17b8932fc75880d63
Acher249 <jchernick2010@gmail.com>
2018-07-05 22:59:10 -0400

    dragula

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,11 +1,11 @@
 `use strict`;
 
 
-$(".characterOne").on("click", function() {
+// $(".characterOne").on("click", function() {
 
-  console.log("This is working");
+//   console.log("This is working");
   
-});
+// });
 
 //$("#result").text(result);
 
@@ -16,3 +16,18 @@ $(".characterOne").on("click", function() {
 //   text: "This is subtext!",
 //   button: "Good Work", 
 // });
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+dragula([$('drag-elements'), $('drop-target')], {
+  revertOnSpill: true
+}).on('drop', function(el) {
+  if ($('drop-target').children.length > 0) {
+    $('display').innerHTML = $('drop-target').innerHTML;
+  } else {
+    $('display').innerHTML = "Display";
+  }
+
+});
