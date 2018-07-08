e26696f6b805a695e861c07582e37b9a71f8af09
Acher249 <jchernick2010@gmail.com>
2018-07-08 01:15:58 -0400

    update

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,36 +1,84 @@
 `use strict`;
 
-
-// $(".characterOne").on("click", function() {
-
-//   console.log("This is working");
-  
-// });
-
-// $("p").click(function(){
-//   alert("The paragraph was clicked.");
-// });
-
-//$("#result").text(result);
-
-
-// swal({
-//   title: "YOU WIN!",
-//   icon: "success",
-//   text: "This is subtext!",
-//   button: "Good Work", 
-// });
-
 function $(id) {
   return document.getElementById(id);
 }
 
-dragula([$('drag-elements'), $('drop-target'), $('drop-target2'), $('drop-target3')], {
+function welcomeAlert() {
+  swal({
+    title: "Get Ready To Battle",
+    icon: "error",
+    text: "Choose Your Champion!",
+    button: "Continue", 
+  });  
+}
+
+setTimeout(welcomeAlert, 1);
+
+
+var pickedChampion = false;
+var pickedOpponent = false;
+
+//this is to feed them objects.
+var champion;
+var opponent;
+
+//turn off drop zone 3
+var dropZone3 = $("drop-target3");
+dropZone3.style.display = "none";
+
+dragula([$('drag-elements'), $('drop-target2'), $('drop-target3')], {
   revertOnSpill: true
 }).on('drop', function(el) {
-  if ($('drop-target').children.length > 0) {
-    $('display').innerHTML = $('drop-target').innerHTML;
-  } else {
-    $('display').innerHTML = "Display";
+
+  // console.log("Drop is working");
+
+  if ($('drag-elements').hasChildNodes = true && pickedChampion == false) {
+
+    //Link choice to JS Object
+    if($('character').innerHTML == "ObiWan") {
+      console.log("champion is ObiWan");
+
+      //Need to figure out how to link choice to object
+      champion = ObiWan;
+    }
+
+    console.log("Picked Champion");
+    setTimeout(pickedChampionFn, 100);
+    pickedChampion = true;
+
+    $(".changeText").text("Enemies");
+    //turn on drop zone 3
+    dropZone3.style.display = "block";
+    //turn background red
   }
+  
+ // Drop Zone 3 
+  if ($('drop-target3').hasChildNodes = true && pickedChampion == true) {
+
+    console.log("Picked Opponent");
+    setTimeout(pickedOpponentFn, 1);
+  }  
 });
+
+
+function pickedChampionFn() {
+  pickedChampion = true;
+  swal({
+        title: "You Picked Your Champion",
+        icon: "info",
+        text: "Now pick your first opponent!",
+        button: "Continue", 
+      });  
+}
+
+function pickedOpponentFn() {
+  pickedOpponent = true;
+  swal({
+        title: "You Picked Your First Opponent",
+        icon: "success",
+        text: "Now you need to fight him!",
+        button: "Continue", 
+      });  
+}
+
